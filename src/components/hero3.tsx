906ef8d3e8a576622c00c97228d7ce00e25073c5
highperfocused <highperfocused@pm.me>
2026-02-01 21:45:58 +0100

    feat: passe Padding der Hero3-Komponente an

--- src/components/hero3.tsx
+++ src/components/hero3.tsx
@@ -72,7 +72,7 @@ const Hero3 = ({
   className,
 }: Hero3Props) => {
   return (
-    <section className={cn("py-32", className)}>
+    <section className={cn("py-16 lg:py-32", className)}>
       <div className="container grid items-center gap-10 lg:grid-cols-2 lg:gap-20">
         <div className="mx-auto flex flex-col items-center text-center md:ml-auto lg:max-w-3xl lg:items-start lg:text-left">
           <h1 className="my-6 text-4xl font-bold text-pretty lg:text-6xl xl:text-7xl">
